91f627908339b52314ac558436df8598fbc2d69c
Yugo <ilhamyugooo@gmail.com>
2024-05-21 20:23:20 +0700

    - login - upgrade error handler - jwt - dotenv - api_doc

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,4 +1,6 @@
 const { User } = require("../models")
+const bcrypt = require('bcryptjs');
+const signToken = require("../helpers/jwt");
 
 class Controller {
     static async test(req, res, next) {
@@ -20,6 +22,43 @@ class Controller {
                 name: user.name,
                 email: user.email
             })
+        } catch (error) {
+            console.log(error)
+            next(error)
+        }
+    }
+
+    //-----Login-----
+    static async postLogin(req, res, next) {
+        try {
+            const { email, password } = req.body
+
+            if (!email) {
+                throw ({ name: "HttpError", status: 400, message: "Email is required" })
+            }
+
+            if (!password) {
+                throw ({ name: "HttpError", status: 400, message: "Password is required" })
+            }
+
+            let user = await User.findOne({
+                where: {
+                    email
+                }
+            })
+
+            if (!user) {
+                throw ({ name: "HttpError", status: 401, message: "Invalid email/password" })
+            }
+
+            let isValidPassword = bcrypt.compareSync(password, user.password);
+            
+            if (!isValidPassword) {
+                throw ({ name: "HttpError", status: 401, message: "Invalid email/password" })
+            }
+
+            let token = signToken({ id: user.id })
+            res.status(200).json({ access_token: token })
         } catch (error) {
             console.log(error)
             next(error)
--- server/middlewares/errorHandlers.js
+++ server/middlewares/errorHandlers.js
@@ -3,6 +3,8 @@ function errorHandlers(err, req, res, next) {
         res.status(400).json({ message: err.errors[0].message })
     } else if (err.name == "SequelizeValidationError") {
         res.status(400).json({ message: err.errors[0].message })
+    } else if (err.name == "HttpError") {
+        res.status(err.status).json({ message: err.message })
     } else {
         res.status(500).json({ message: "Internal server error" })
     }
