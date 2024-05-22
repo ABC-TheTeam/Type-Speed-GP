1a9bb272847668b53c7f071b997ac80578c0c0f7
Muhammad Mushlih Al 'Aadil <mmushlih.alaadil@gmail.com>
2024-05-22 10:02:38 +0700

    rubah origin

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const { Server } = require("socket.io");
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000/",
+    origin: "*",
   },
 });
 
