d5741005dc1d19f24ddb28bb10d7b468e0c30876
Muhammad Mushlih Al 'Aadil <mmushlih.alaadil@gmail.com>
2024-05-22 09:52:06 +0700

    socket-io chat global

--- server/app.js
+++ server/app.js
@@ -1,10 +1,52 @@
-const express = require('express');
-const app = express()
-require('dotenv').config()
-const router = require('./routes/route');
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const router = require("./routes/route");
+const cors = require("cors");
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
+// Socket Settings
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000/",
+  },
+});
+
+io.on("connection", (socket) => {
+    // io.emit("new-connect")
+    // console.log(socket.id, "Socket ID");
+
+    socket.on("set-user", (username) => {
+        socket.user = {
+            username
+        }
+    })
+
+    // terima pesan dari client
+    socket.on("send-message", (payload) => {
+        // pesan yang diterima di-publish ke seluruh client
+        io.emit("new-message", payload)
+    })
+});
+
+// Body Parser and CORS
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(router)
+app.use(cors());
 
-module.exports = app
+// Routing
+app.use(router);
+
+app.get("/", (req, res) => {
+  res.send("Hello, welcome to our server!");
+});
+
+httpServer.listen(port, () => {
+  console.clear();
+  console.log(`Server is running on port ${port}`);
+});
+
+// module.exports = app;
